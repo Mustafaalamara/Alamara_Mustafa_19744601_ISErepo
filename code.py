2b9127dfc6a188a6a6f0a21a995605b899d3bb61
Mustafaalamara <19744601@student.curtin.edu.au>
2022-06-05 18:25:21 +0800

    TestMycode.py adjusted

--- code.py
+++ code.py
@@ -1,15 +1,10 @@
 def lower_upper(string, ch):
-    #print('''1- For lowercase
-#2- For uppercase
-#''')
-    #ch = int(input("Choose: "))
     if ch == 1:
         n = string.lower()
     elif ch == 2:
         n = string.upper()
     print(n)
     return n
-#convert_to_upper(input("Enter: "))
 
 def containsNumber(string):
     for a in string:
@@ -20,15 +15,12 @@ def containsNumber(string):
             a = False
             print("It doesn't contain numbers")
     return a
-#print(containsNumber(input("ghh: ")))
 
 def valid_number(string):
     for a in string:
         if a.isnumeric():
             return True
     return False
-#print(valid_number(input("Num: ")))
-
 
 def remove_and_change(string, ch):
     for a in string:
@@ -39,7 +31,6 @@ def remove_and_change(string, ch):
     elif ch == 2:
             ns = string.upper()
     print(ns) 
-#remove_and_change("KASDas334JA", 1)
 
 def meters_to_feet(meters):
     return meters * 3.281
--- testMyCode.py
+++ testMyCode.py
@@ -1,8 +1,61 @@
 import code
-def testconversion():
-    actual = code.cm_inches(1, 10)
-    assert 13 == actual
+def testupper_lower():
+    actual = code.lower_upper("TESTMYCODE",1)
+    assert actual, "testmycode"
+    
+    actual = code.lower_upper("calculator", 2)
+    assert actual, "CALCULATOR"
+    
+def test_containsNumber():
+    actual = code.containsNumber("73Abdut77uial")
+    assert actual
+    
+    actual = code.containsNumber("abcDEsdf")
+    assert not actual
+    
+def test_Valid_Number():
+    actual = code.valid_number("897")
+    assert actual
+    
+    actual = code.valid_number("asdaf")
+    assert not actual
+    
+def test_remove_and_change():
+    actual = code.remove_and_change("BAA3AAD", 1)
+    assert "baaaad" == actual, "Including digits and ch = 1"
+    
+    actual = code.remove_and_change("4ftern00n", 2)
+    assert "FTERNN" == actual, "Including digits and ch = 2"
+    
+    actual = code.remove_and_change("STRUCTURE", 1)
+    assert "structure" == actual, "Doesn't include digits and ch = 1"
+    
+    actual = code.remove_and_change("assignment", 2)
+    assert "ASSIGNMENT" == actual, "Doesn't include digits and ch = 2"
+    
+def test_meter_to_feet():
+    actual = code.meters_to_feet(2)
+    assert 6.562 == actual
+    
+def test_feet_to_meters():
+    actual = code.feet_to_meters(13.124)
+    assert 4 == actual
+    
+def test_cm_to_inches():
+    actual = code.cm_to_inches(15)
+    assert  5.905 == actual
+    
+def test_inches_to_cm():
+    actual = code.meters_to_feet(8)
+    assert 20.32 == actual
     
     
 if __name__ == "__main__":
-    testconversion()
+    testupper_lower()
+    test_containsNumber()
+    test_Valid_Number()
+    test_remove_and_change()
+    test_meter_to_feet()
+    test_feet_to_meters()
+    test_cm_to_inches()
+    test_inches_to_cm()
